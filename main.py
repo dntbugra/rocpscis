4f5d2e121bf528f5d8167a1bc6d9cb06dd9cb156
cengizozel <60388555+cengizozel@users.noreply.github.com>
2022-09-08 18:16:42 -0400

    Added game loop

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import random
 
 def win():
-    print()
-    print("You win!")
+    print("\nYou win!")
 def draw():
-    print()
-    print("It's a draw!")
+    print("\nIt's a draw!")
 def lose():
-    print()
-    print("You lose!")
+    print("\nYou lose!")
 
 moves=("Rock" , "Paper" , "Scissors")
 
@@ -18,33 +15,35 @@ a=moves[0]
 b=moves[1]
 c=moves[2]
 
-print()
-print("Welcome")
-print()
-print("Press q for -> 'rock', w for 'paper' , e for 'scissors'")
-print()
+print("\nWelcome\n")
 
-pm=input()
+def play():
+    print("Press q for -> 'rock', w for 'paper' , e for 'scissors'\n")
 
-if x==a:
-    if pm=="q":
-        draw()
-    elif pm=="w":
-        win()
-    elif pm=="e":
-        lose()
-if x==b:
-    if pm=="q":
-        lose()
-    elif pm=="w":
-        draw()
-    elif pm=="e":
-        win()
-if x==c:
-    if pm=="q":
-        win()
-    elif pm=="w":
-        lose()
-    elif pm=="e":
-        draw()
-print()
+    pm=input()
+
+    if x==a:
+        if pm=="q":
+            draw()
+        elif pm=="w":
+            win()
+        elif pm=="e":
+            lose()
+    if x==b:
+        if pm=="q":
+            lose()
+        elif pm=="w":
+            draw()
+        elif pm=="e":
+            win()
+    if x==c:
+        if pm=="q":
+            win()
+        elif pm=="w":
+            lose()
+        elif pm=="e":
+            draw()
+    print("=" * 50 + "\n")
+
+while True:
+    play()
